cb76ef8387e441a7a39dcf15d214fd4be3bd9ac6
wasimmostakim2965-ui <wasimmostakim2965@gmail.com>
2026-09-17 21:16:25 +0600

    Load onboarding and responsive dashboard fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './fixes.css';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
