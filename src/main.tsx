d0ed0ec142d75c47704c457bbe4d5d84ebc8cf9a
wasimmostakim2965-ui <wasimmostakim2965@gmail.com>
2026-09-19 09:17:51 +0600

    Load shared Paywai brand styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './fixes.css';
+import './brand.css';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
