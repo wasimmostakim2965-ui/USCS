8088020850c9bfa4f20ae33d9e460eebed65609a
wasimmostakim2965-ui <wasimmostakim2965@gmail.com>
2026-09-21 19:12:04 +0600

    fix(auth): add dedicated OAuth callback route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,13 @@ import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
+import Home, { AuthCallback } from "./pages/Home";
 
 function Router() {
   // make sure to consider if you need authentication for certain routes
   return (
     <Switch>
+      <Route path={"/auth/callback"} component={AuthCallback} />
       <Route path={"/"} component={Home} />
       <Route path={"/dashboard"} component={Home} />
       <Route path={"/404"} component={NotFound} />
